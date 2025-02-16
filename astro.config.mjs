54084063dec69312e88213d1a4243a63bd823569
honda keiu <k-honda@studio-spoon.co.jp>
2025-02-16 16:40:24 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hondakeiu.github.io/buzz-bites",
+  site: "https://hondakeiu.github.io",
+  base: "buzz-bites",
   vite: {
     css: {
       preprocessorOptions: {
