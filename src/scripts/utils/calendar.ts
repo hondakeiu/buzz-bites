a6ccb23ebc27bbd2c54626f6da070825a50366b3
honda keiu <k-honda@studio-spoon.co.jp>
2025-02-17 03:42:43 +0900

    修正

--- src/scripts/utils/calendar.ts
+++ src/scripts/utils/calendar.ts
@@ -25,6 +25,7 @@ export const getYoubi = (date: string) => {
 
 export const getTodaySectionId = () => {
   const today = new Date();
+  console.log(today);
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, "0");
   const day = String(today.getDate()).padStart(2, "0");
